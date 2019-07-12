c4374332b4971b596a019f9106067e99821a5331
Diego Paez <paez.di@gmail.com>
2019-07-12 20:13:16 -0300

    Update: wip config

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 'use strict';
+const {join} = require('path');
 const Store = require('electron-store');
 
 module.exports = new Store({
   defaults: {
-    favoriteAnimal: '🦄'
+    keys: [
+      'dat://af206247d57b0cdba9bb54b17773d258dce4d6570b7e1c6982e7aadb6f920be2'
+    ],
+    directories: {
+      main: join(__dirname, 'glp_main'),
+      dest: join(__dirname, 'glp_archives')
+    }
   }
 });
