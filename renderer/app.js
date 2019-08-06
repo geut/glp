806251e9cc75e6edcb9c507da859312ba5fa7dde
Diego Paez <paez.di@gmail.com>
2019-08-06 19:24:00 -0300

    Update: layout improvements for the new view: writter mode

--- renderer/app.js
+++ renderer/app.js
@@ -1,11 +1,22 @@
 import React, {useState, useEffect} from 'react';
 
 import {makeStyles} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Fade from '@material-ui/core/Fade';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import EditIcon from '@material-ui/icons/Edit';
+import FoldersIcon from '@material-ui/icons/Notes';
 
 import {switchcase} from 'common/utils';
+import GEUT from './components/logo';
 import Config from '../config';
 import {useStateValue} from './hooks';
+import Edit from './containers/edit';
 import Home from './containers/home';
 import Loader from './containers/loader';
 import Detail from './containers/detail';
@@ -14,14 +25,36 @@ if (module.hot) {
   module.hot.accept();
 }
 
+const drawerWidth = 140;
+
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    display: 'flex',
   },
+  logo: {
+    width: '80px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    margin: theme.spacing(1)
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: theme.mixins.toolbar,
   title: {
     textAlign: 'right',
     padding: theme.spacing(2),
     right: '1em'
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+  },
+  center: {
+    textAlign: 'center'
   }
 }));
 
@@ -55,14 +88,68 @@ function AppContainer() {
     getContent();
   }, []);
 
+
+  const actions = [
+    {
+      action: 'Edit',
+      icon: () => <EditIcon/>,
+      activePage: 'edit',
+      description: 'Write down some thoughts'
+    },
+    {
+      action: 'Navigate',
+      icon: () => <FoldersIcon/>,
+      activePage: 'home',
+      description: 'Navigate ideas'
+    }
+  ];
+
+  const handleRoute = activePage => {
+    dispatch({type: 'GOTO', activePage});
+  };
+
+  const drawer = (
+    <div className={classes.center}>
+      <GEUT className={classes.logo}/>
+      <Divider />
+      <List>
+        {actions.map(({action, icon, activePage}) => (
+          <ListItem button key={action} onClick={() => handleRoute(activePage)}>
+            <ListItemIcon>{icon()}</ListItemIcon>
+            <ListItemText primary={action} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
+  );
+
   return (
-    <main className={classes.root}>
-      {switchcase({
-        loading: () => <Loader/>,
-        home: () => <Fade in><Home mainKey={mainArchive} extra={extArchives} metadata={metadata}/></Fade>,
-        detail: () => <Fade in><Detail fileData={fileData}/></Fade>
-      })(<Loader/>)(activePage)}
-    </main>
+    <div className={classes.root}>
+      <CssBaseline />
+      <nav className={classes.drawer} aria-label="glp actions">
+        <Drawer
+          open
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <main className={classes.content}>
+        {switchcase({
+          loading: () => <Fade in timeout={{enter: 200, exit: 200}}><Loader/></Fade>,
+          edit: () => <Fade in timeout={{enter: 200, exit: 200}}><Edit /></Fade>,
+          home: () => <Fade in timeout={{enter: 200, exit: 200}}><Home mainKey={mainArchive} extra={extArchives} metadata={metadata}/></Fade>,
+          detail: () => <Fade in timeout={{enter: 200, exit: 200}}><Detail fileData={fileData}/></Fade>
+        })(<Loader/>)(activePage)}
+      </main>
+    </div>
   );
 }
 
